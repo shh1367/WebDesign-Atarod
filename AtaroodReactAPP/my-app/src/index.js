d8b02c9f3fc911a3b5322d6b2a4ea1bcc454db35
newHamed <shh1367@yahoo.com>
2023-07-12 19:38:26 +0330

    #5 after some changes

--- AtaroodReactAPP/my-app/src/index.js
+++ AtaroodReactAPP/my-app/src/index.js
@@ -4,14 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// const tick = () => {
+//   const elem = (
+//     <div>
+//       <h1>
+//         hello 
+//       </h1>
+
+//       <h2>
+//         the time is : {new Date().toLocaleTimeString()}
+//       </h2>
+
+//     </div>
+//   )
+// }
+
+// setInterval(()=>{
+//   tick();
+// } , 1000);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+
+
+
+// ReactDOM.render(elem  , document.getElementById('root'));
